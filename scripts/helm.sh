abafda66b4733c7cbd14661bd45841e4e51b7a56
mdabuateef <mdabuateef@gmail.com>
2024-02-17 22:37:00 +0530

    changees made11

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -5,7 +5,6 @@ eksctl create iamserviceaccount --name ebs-csi-controller-sa --namespace kube-sy
 SERVICE_ACCOUNT_ROLE_ARN=$(aws iam get-role --role-name AmazonEKS_EBS_CSI_DriverRole --output json | jq -r '.Role.Arn')
 
 eksctl create addon --name aws-ebs-csi-driver --cluster spring --service-account-role-arn eksctl create addon --name aws-ebs-csi-driver --cluster spring --service-account-role-arn "$SERVICE_ACCOUNT_ROLE_ARN" --force
- --force
 
 helm upgrade -i kubecost oci://public.ecr.aws/kubecost/cost-analyzer --version="1.104.4" --namespace kubecost --create-namespace -f https://raw.githubusercontent.com/kubecost/cost-analyzer-helm-chart/develop/cost-analyzer/values-eks-cost-monitoring.yaml --set prometheus.configmapReload.prometheus.enabled="false"
 
