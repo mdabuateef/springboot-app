4e40d381231c3a7c93ea7c5f814468f211306629
mdabuateef <mdabuateef@gmail.com>
2024-02-17 23:09:29 +0530

    changees made11

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -1,11 +1,11 @@
 eksctl utils associate-iam-oidc-provider --cluster spring --approve
 
-eksctl create iamserviceaccount --name ebs-csi-controller-sa --namespace kube-system --cluster spring --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy --approve --role-only --role-name AmazonEKS_EBS_CSI_DriverRole
+eksctl create iamserviceaccount --name ebs-csi-controller-sa --namespace kube-system --cluster spring --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy --approve --role-name AmazonEKS_EBS_CSI_DriverRole
 
 SERVICE_ACCOUNT_ROLE_ARN=$(aws iam get-role --role-name AmazonEKS_EBS_CSI_DriverRole --output json | jq -r '.Role.Arn')
 
 eksctl create addon --name aws-ebs-csi-driver --cluster spring --service-account-role-arn eksctl create addon --name aws-ebs-csi-driver --cluster spring --service-account-role-arn "$SERVICE_ACCOUNT_ROLE_ARN" --force
 
 helm upgrade -i kubecost oci://public.ecr.aws/kubecost/cost-analyzer --version="1.104.4" --namespace kubecost --create-namespace -f https://raw.githubusercontent.com/kubecost/cost-analyzer-helm-chart/develop/cost-analyzer/values-eks-cost-monitoring.yaml --set prometheus.configmapReload.prometheus.enabled="false"
-
+ 
  kubectl patch svc kubecost-cost-analyzer -n kubecost -p '{"spec": {"type": "LoadBalancer"}}'
