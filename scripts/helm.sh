942a540477afd69e14bb47f56f2177c5aeb67347
mdabuateef <mdabuateef@gmail.com>
2024-02-17 23:16:53 +0530

    changees made11

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -6,6 +6,13 @@ SERVICE_ACCOUNT_ROLE_ARN=$(aws iam get-role --role-name AmazonEKS_EBS_CSI_Driver
 
 eksctl create addon --name aws-ebs-csi-driver --cluster spring --service-account-role-arn eksctl create addon --name aws-ebs-csi-driver --cluster spring --service-account-role-arn "$SERVICE_ACCOUNT_ROLE_ARN" --force
 
+wget https://get.helm.sh/helm-v3.9.3-linux-amd64.tar.gz
+tar xvf helm-v3.9.3-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin
+sudo rm helm-v3.4.1-linux-amd64.tar.gz
+sudo rm -rf linux-amd64
+helm version
+
 helm upgrade -i kubecost oci://public.ecr.aws/kubecost/cost-analyzer --version="1.104.4" --namespace kubecost --create-namespace -f https://raw.githubusercontent.com/kubecost/cost-analyzer-helm-chart/develop/cost-analyzer/values-eks-cost-monitoring.yaml --set prometheus.configmapReload.prometheus.enabled="false"
  
  kubectl patch svc kubecost-cost-analyzer -n kubecost -p '{"spec": {"type": "LoadBalancer"}}'
